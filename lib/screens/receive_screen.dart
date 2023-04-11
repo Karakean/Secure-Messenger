8c0e736b26a4651ef650da5297af574965484c21
jan-Kulpas <krzychu3813@gmail.com>
2023-04-11 18:22:45 +0200

    create files for screens

---
+++ lib/screens/receive_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ReceiveScreen extends StatelessWidget {
+  const ReceiveScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
