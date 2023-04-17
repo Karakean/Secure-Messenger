c9ba7617ef530ec1534931e4dd24f4fb6269423d
jan-Kulpas <krzychu3813@gmail.com>
2023-04-17 21:30:50 +0200

    add register option

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,17 +1,11 @@
 import 'dart:io';
 import 'dart:math';
 
-
-
 import 'package:flutter/material.dart';
 import 'package:secure_messenger/tmp.dart';
 
-
 import '../models/session.dart';
 import '../models/rsa_key_helper.dart';
-
-
-
 
 class LoginScreen extends StatefulWidget {
   static const routeName = "/login";
@@ -22,43 +16,50 @@ class LoginScreen extends StatefulWidget {
   State<LoginScreen> createState() => _LoginScreenState();
 }
 
-
-
 class _LoginScreenState extends State<LoginScreen> {
-  late final TextEditingController _controller;
+  final _formKey = GlobalKey<FormState>();
+  late final TextEditingController _passwordController;
+
+  bool _isLogin = true;
+  String _login = '';
+  String _password = '';
 
   @override
   void initState() {
     super.initState();
-    _controller = TextEditingController();
+    _passwordController = TextEditingController();
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    _passwordController.dispose();
     super.dispose();
   }
 
   void _submit() {
-  
-  NetworkInterface.list().then((interfaces) {
-    for (var interface in interfaces) {
-      print('Name: ${interface.name}');
-      for (var addr in interface.addresses) {
-        print('Addr: ${addr}');
+    NetworkInterface.list().then((interfaces) {
+      for (var interface in interfaces) {
+        print('Name: ${interface.name}');
+        for (var addr in interface.addresses) {
+          print('Addr: ${addr}');
+        }
       }
-    }
-  RsaKeyHelper rsaKeyHelper = RsaKeyHelper();
-  var keyPair = rsaKeyHelper.generateRSAkeyPair(rsaKeyHelper.exampleSecureRandom());
-  rsaKeyHelper.saveKeysToFiles(keyPair);
-  rsaKeyHelper.loadKeysFromFiles();
-  });
-  if (_controller.text.trim() == "xd") {
-    // ? dziwne ale wychodzi na to ze menu jest by default na stacku pod loginem
-    Navigator.of(context).pop();
-  }
-  }
+      RsaKeyHelper rsaKeyHelper = RsaKeyHelper();
+      var keyPair = rsaKeyHelper.generateRSAkeyPair(rsaKeyHelper.exampleSecureRandom());
+      rsaKeyHelper.saveKeysToFiles(keyPair);
+      rsaKeyHelper.loadKeysFromFiles();
+    });
 
+    if (!_formKey.currentState!.validate()) {
+      return; //nie wyslo
+    }
+    _formKey.currentState!.save();
+
+    if (_password == "xdxd") {
+      // ? dziwne ale wychodzi na to ze menu jest by default na stacku pod loginem
+      Navigator.of(context).pop();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -71,30 +72,70 @@ class _LoginScreenState extends State<LoginScreen> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
-                "Select an interface:",
+                "Secure Messenger",
                 style: Theme.of(context).textTheme.headlineSmall,
               ),
-              Text(
-                "Select an interface:",
-                style: Theme.of(context).textTheme.headlineSmall,
-              ),
-              Text(
-                "Enter your password:",
-                style: Theme.of(context).textTheme.headlineSmall,
-              ),
-              Card(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: TextField(
-                    obscureText: true,
-                    controller: _controller,
-                    onSubmitted: (_) => _submit(),
-                    textInputAction: TextInputAction.done,
-                    keyboardType: TextInputType.visiblePassword,
-                    decoration: const InputDecoration(
-                      hintText: "Password",
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    CustomField(
+                      visible: !_isLogin,
+                      child: TextFormField(
+                        enabled: !_isLogin,
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Username",
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty || value.characters.length < 4) {
+                            return "Enter at least 4 characters";
+                          }
+                          return null;
+                        },
+                        onSaved: (newValue) {
+                          _login = newValue!;
+                        },
+                      ),
                     ),
-                  ),
+                    CustomField(
+                      child: TextFormField(
+                        enabled: true,
+                        controller: _passwordController,
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Password",
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty || value.characters.length < 4) {
+                            return "Enter at least 4 characters";
+                          }
+                          return null;
+                        },
+                        onSaved: (newValue) {
+                          _password = newValue!;
+                        },
+                      ),
+                    ),
+                    CustomField(
+                      visible: !_isLogin,
+                      child: TextFormField(
+                        enabled: !_isLogin,
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Repeat password",
+                        ),
+                        validator: (value) {
+                          if (_passwordController.text != value) {
+                            return "Passwords do not match";
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                  ],
                 ),
               ),
               ElevatedButton(
@@ -104,10 +145,41 @@ class _LoginScreenState extends State<LoginScreen> {
                   foregroundColor: Colors.black,
                   elevation: 10,
                 ),
-                child: const Text("Submit"),
+                child: Text(_isLogin ? "Log in" : "Register"),
+              ),
+              TextButton(
+                onPressed: () {
+                  setState(() {
+                    _isLogin = !_isLogin;
+                  });
+                },
+                child: Text(
+                  _isLogin ? "I want to register." : "I want to log in.",
+                  style: const TextStyle(color: Colors.black),
+                ),
               ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomField extends StatelessWidget {
+  final Widget child;
+  final bool visible;
+
+  const CustomField({this.visible = true, required this.child, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: BoxConstraints(maxHeight: visible ? double.infinity : 0),
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: child,
         ),
       ),
     );
